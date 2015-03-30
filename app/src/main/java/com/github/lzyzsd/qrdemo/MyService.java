ec782f6cab53a255a47abf167a01fbcaae25d4ea
Bruce Lee <zhenyue.li@baidao.com>
2015-03-30 23:56:17 +0800

    init

---
+++ app/src/main/java/com/github/lzyzsd/qrdemo/MyService.java
@@ -0,0 +1,11 @@
+package com.github.lzyzsd.qrdemo;
+
+import com.loopj.android.http.*;
+/**
+ * Created by bruce on 15/3/30.
+ */
+public class MyService {
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+}
