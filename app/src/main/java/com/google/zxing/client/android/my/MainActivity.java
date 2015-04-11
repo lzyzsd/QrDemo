3ffd8360813351147c95be273db2b95bc292f40d
Bruce Lee <zhenyue.li@baidao.com>
2015-04-11 21:26:29 +0800

    add tabs

--- app/src/main/java/com/google/zxing/client/android/my/MainActivity.java
+++ app/src/main/java/com/google/zxing/client/android/my/MainActivity.java
@@ -1,190 +1,106 @@
 package com.google.zxing.client.android.my;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
+import android.widget.FrameLayout;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.android.R;
-import com.google.zxing.client.android.clipboard.ClipboardInterface;
-import com.squareup.okhttp.OkHttpClient;
-
-import org.joda.time.DateTime;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import retrofit.RestAdapter;
-import retrofit.client.OkClient;
-import retrofit.converter.GsonConverter;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.android.observables.AndroidObservable;
-
 
 public class MainActivity extends ActionBarActivity {
-    Button searchButton;
-    EditText nameText;
-    ProgressBar progressBar;
-    Spinner typeSpinner;
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
+    FrameLayout container;
+    View[] tabs = new View[3];
+    Fragment[] tabFragments = new Fragment[3];
+    private int selectedTab = 0;
 
-    MyService myService;
-
-    Subscription searchByKeeperSubscription, searchByNameSubScription, searchByIdSubscription;
-
-    private void initMyService() {
-        RestAdapter.Builder builder = new RestAdapter.Builder();
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(DateTime.class, new Product.DateTimeTypeAdapter())
-                .create();
-        OkHttpClient okHttpClient = new OkHttpClient();
-        okHttpClient.setConnectTimeout(60, TimeUnit.SECONDS);
-        okHttpClient.setReadTimeout(30, TimeUnit.SECONDS);
-        OkClient okClient = new OkClient(okHttpClient);
-
-        builder.setClient(okClient);
-        RestAdapter restAdapter = builder.setClient(new OkClient())
-                .setEndpoint("http://" + Constants.IP + ":" + Constants.PORT)
-                .setLogLevel(RestAdapter.LogLevel.HEADERS)
-                .setConverter(new GsonConverter(gson))
-                .build();
-
-        myService = restAdapter.create(MyService.class);
-    }
+    private static final String STATE_SELECTED_TAB = "selected_tab";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initMyService();
+        container = (FrameLayout) findViewById(R.id.content_container);
+        if (null == savedInstanceState) {
+            SearchTabFragment searchTabFragment = new SearchTabFragment();
+            ObsoleteTabFragment obsoleteTabFragment = new ObsoleteTabFragment();
+            MeTabFragment meTabFragment = new MeTabFragment();
 
-        searchButton = (Button) findViewById(R.id.btn_search);
-        nameText = (EditText) findViewById(R.id.et_name);
-        progressBar = (ProgressBar) findViewById(R.id.pb_loading);
-        recyclerView = (RecyclerView) findViewById(R.id.rv_recycler_view);
-        typeSpinner = (Spinner) findViewById(R.id.s_search_type);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-            R.array.search_by_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        typeSpinner.setAdapter(adapter);
+            getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.content_container, searchTabFragment, "search")
+                .hide(searchTabFragment)
+                .add(R.id.content_container, obsoleteTabFragment, "obsolete")
+                .hide(obsoleteTabFragment)
+                .add(R.id.content_container, meTabFragment, "me")
+                .hide(meTabFragment)
+                .commit();
+            getSupportFragmentManager().executePendingTransactions();
+        } else {
+            selectedTab = savedInstanceState.getInt(STATE_SELECTED_TAB);
+        }
 
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(new MyAdapter());
+        tabs[0] = findViewById(R.id.tab_search);
+        tabs[1] = findViewById(R.id.tab_obsolete);
+        tabs[2] = findViewById(R.id.tab_me);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        tabFragments[0] = getSupportFragmentManager().findFragmentByTag("search");
+        tabFragments[1] = getSupportFragmentManager().findFragmentByTag("obsolete");
+        tabFragments[2] = getSupportFragmentManager().findFragmentByTag("me");
+
+        selectTab(selectedTab);
+        addTabSelectListener();
+    }
+
+    private void selectTab(int index) {
+        tabs[selectedTab].setSelected(false);
+        tabs[index].setSelected(true);
+
+        if (index == selectedTab) {
+            if (!tabFragments[index].isVisible()) {
+                getSupportFragmentManager()
+                    .beginTransaction()
+                    .show(tabFragments[index])
+                    .commit();
+            }
+        } else {
+            getSupportFragmentManager()
+                .beginTransaction()
+                .show(tabFragments[index])
+                .hide(tabFragments[selectedTab])
+                .commit();
+        }
+        getSupportFragmentManager().executePendingTransactions();
+
+        selectedTab = index;
+    }
+
+    private void addTabSelectListener() {
+        tabs[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameText.getText().toString();
-                progressBar.setVisibility(View.VISIBLE);
-                if ((typeSpinner.getSelectedItem()).equals("保管人")) {
-                    searchByKeeperName(name);
-                } else {
-                    searchByProductName(name);
-                }
+                selectTab(0);
             }
         });
-    }
 
-    private void unsubscribe() {
-        if (searchByIdSubscription != null) {
-            searchByIdSubscription.unsubscribe();
-        }
+        tabs[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectTab(1);
+            }
+        });
 
-        if (searchByNameSubScription != null) {
-            searchByNameSubScription.unsubscribe();
-        }
-
-        if (searchByKeeperSubscription != null) {
-            searchByKeeperSubscription.unsubscribe();
-        }
-    }
-
-    private void searchByKeeperName(String name) {
-        unsubscribe();
-        searchByKeeperSubscription = AndroidObservable.bindActivity(this, myService.searchByKeeperName(name))
-                .subscribe(new Subscriber<List<Product>>() {
-                    @Override
-                    public void onCompleted() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        progressBar.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onNext(List<Product> products) {
-                        progressBar.setVisibility(View.GONE);
-                        ((MyAdapter) recyclerView.getAdapter()).setProducts(products);
-                    }
-                });
-    }
-
-    private void searchByProductName(String name) {
-        unsubscribe();
-        searchByNameSubScription = AndroidObservable.bindActivity(this, myService.searchByProductName(name))
-                .subscribe(new Subscriber<List<Product>>() {
-                    @Override
-                    public void onCompleted() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        progressBar.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onNext(List<Product> products) {
-                        progressBar.setVisibility(View.GONE);
-                        ((MyAdapter) recyclerView.getAdapter()).setProducts(products);
-                    }
-                });
-    }
-
-    private void searchById(int id) {
-        unsubscribe();
-        nameText.setText("");
-        searchByIdSubscription = AndroidObservable.bindActivity(this, myService.searchById(id))
-                .subscribe(new Subscriber<Product>() {
-                    @Override
-                    public void onCompleted() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        progressBar.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onNext(Product product) {
-                        progressBar.setVisibility(View.GONE);
-                        List<Product> products = new ArrayList<>(1);
-                        products.add(product);
-                        ((MyAdapter) recyclerView.getAdapter()).setProducts(products);
-                    }
-                });
+        tabs[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectTab(2);
+            }
+        });
     }
 
     @Override
@@ -200,20 +116,5 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Constants.REQUEST_CODE_CAPTURE_ACTIVITY && !TextUtils.isEmpty(ClipboardInterface.getText(this))) {
-            String url = ClipboardInterface.getText(this).toString();
-            Uri uri = Uri.parse(url);
-            if (uri.getHost() == null || !uri.getHost().equals(Constants.IP)) {
-                return;
-            }
-            List<String> paths = uri.getPathSegments();
-            int id = Integer.parseInt(paths.get(paths.size() - 1));
-            searchById(id);
-        }
     }
 }
